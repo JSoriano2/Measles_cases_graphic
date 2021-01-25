a8eced1d8090fb9140a7c8f64452530f0bf3e94d
Jaime <jaime.soriano01@estudiant.upf.edu>
2021-01-25 11:01:15 +0100

    Changing the graph

--- index.R
+++ index.R
@@ -7,8 +7,9 @@ names(dat)[2] <- "France"
 names(dat)[3] <- "Spain"
 meses <- row.names(dat)
 mf <- factor(meses,levels=c("Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec","Jan", "Feb", "Mar"))
-plot(dat$Bulgaria ~ mf,xaxt="n",ylim=c(1,700), type="o", col="green", lwd=5, main="Casos Sarampion (Abril 2019 - Marzo 2020)", xlab="Mes", ylab= 'Numero de casos', cex.lab=1.2)
+plot(dat$Bulgaria ~ mf,xaxt="n",ylim=c(1,700), type="o", col="dark green", lwd=5, main="Casos Sarampión (Abril 2019 - Marzo 2020)", xlab="Mes", ylab= 'Número de casos', cex.lab=1.2)
 lines(dat$Spain  ~ mf,type="o", col="red", lwd=5 )
 lines(dat$France ~ mf,type="o", col="blue", lwd=5 )
 axis(1,at=1:12,labels=meses)
-legend(1, 725, c("Francia","Bulgaria","España"), fill=c("blue","green","red"), box.lty = 0, text.font = 4)
+legend(1, 725, c("Francia","Bulgaria","España"), fill=c("blue","dark green","red"), box.lty = 0, text.font = 4)
+
